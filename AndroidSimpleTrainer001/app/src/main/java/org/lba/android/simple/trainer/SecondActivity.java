0b5c871ef5f4bda6101507f3880e023286f44f3d
d3vsupp0r7 <dev@dev.com>
2019-08-03 14:21:44 +0200

    Tech Feature - Button for show simple popup - 1

--- AndroidSimpleTrainer001/app/src/main/java/org/lba/android/simple/trainer/SecondActivity.java
+++ AndroidSimpleTrainer001/app/src/main/java/org/lba/android/simple/trainer/SecondActivity.java
@@ -2,16 +2,32 @@ package org.lba.android.simple.trainer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.content.Context;
+import android.view.ViewGroup.LayoutParams;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import org.lba.android.simple.trainer.costraints.ApplicationCostraintsEnum;
 
 public class SecondActivity extends Activity {
 
     Button backToFirstActivityButton;
+    Button showPopupButton;
+    PopupWindow mPopupWindow;
+    //
+    Context mContext;
+    ConstraintLayout mConstraintLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +37,9 @@ public class SecondActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
 
+        mConstraintLayout = (ConstraintLayout) findViewById(R.id.secondActivityLayout);
+
+        /*Activity Navigation*/
         backToFirstActivityButton =(Button)findViewById(R.id.backToFirstActivityButton);
         backToFirstActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +47,41 @@ public class SecondActivity extends Activity {
                 Log.d(ApplicationCostraintsEnum.APP_NAME.getValue(),"** SecondActivity goto MainActivity - started **");
                 Intent intent= new Intent(SecondActivity.this,MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        /**/
+        mContext = this;
+        showPopupButton = (Button)findViewById(R.id.showPopupButton);
+        showPopupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+                View customView = inflater.inflate(R.layout.simple_popup_layout,null);
+                //
+
+                Drawable d = new ColorDrawable(Color.BLUE);
+                //
+                mPopupWindow = new PopupWindow(
+                        customView,
+                        LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT
+                );
+
+                // Get a reference for the custom view close button
+                ImageButton closeButton = (ImageButton) customView.findViewById(R.id.ib_close);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        // Dismiss the popup window
+                        mPopupWindow.dismiss();
+                    }
+                });
+
+                mPopupWindow.setBackgroundDrawable(d);
+                mPopupWindow.showAtLocation(mConstraintLayout, Gravity.CENTER,0,0);
             }
         });
     }
