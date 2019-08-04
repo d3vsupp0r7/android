fa0d6849c0310e6db8aca1e83c128ae199a9ac9e
d3vsupp0r7 <dev@dev.com>
2019-08-04 14:46:43 +0200

    RFI - Refactor - Added time pattern enum

---
+++ OrariTreni/app/src/main/java/org/sglba/trainman/costraints/DatePatternFormatterCostraintEnum.java
@@ -0,0 +1,20 @@
+package org.sglba.trainman.costraints;
+
+public enum DatePatternFormatterCostraintEnum {
+
+    US_DATE_PATTERN_WITH_TIME("yyyy-MM-dd'T'HH:mm:ss"),
+    US_DATE_PATTERN("yyyy-MM-dd"),
+    EU_DATE_PATTERN_WITH_TIME("dd-MM-yyyy'T'HH:mm:ss"),
+    EU_DATE_PATTERN("dd-MM-yyyy"),
+    ;
+
+    private String value;
+
+    DatePatternFormatterCostraintEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
