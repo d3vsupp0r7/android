e5a86039af0472f626a54f3b4161d66d261d9e22
Aniello La Bua <aniello.labua@alliance-healthcare.net>
2019-08-13 16:59:26 +0200

    Update UI exchange bean

---
+++ OrariTreni/app/src/main/java/org/sglba/trainman/ui/model/TrainSolution.java
@@ -0,0 +1,57 @@
+package org.sglba.trainman.ui.model;
+
+import org.sglba.trainman.model.Soluzioni;
+import org.sglba.trainman.model.Vehicle;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class TrainSolution implements Serializable {
+
+    private Soluzioni solution;
+    private Vehicle firstVehicle;
+    private Vehicle lastVehicle;
+    private List<Vehicle> VehicleForSolution;
+
+    public TrainSolution() {
+    }
+
+    public TrainSolution(Soluzioni solution, Vehicle firstVehicle, Vehicle lastVehicle, List<Vehicle> vehicleForSolution) {
+        this.solution = solution;
+        this.firstVehicle = firstVehicle;
+        this.lastVehicle = lastVehicle;
+        VehicleForSolution = vehicleForSolution;
+    }
+
+    public Soluzioni getSolution() {
+        return solution;
+    }
+
+    public void setSolution(Soluzioni solution) {
+        this.solution = solution;
+    }
+
+    public Vehicle getFirstVehicle() {
+        return firstVehicle;
+    }
+
+    public void setFirstVehicle(Vehicle firstVehicle) {
+        this.firstVehicle = firstVehicle;
+    }
+
+    public Vehicle getLastVehicle() {
+        return lastVehicle;
+    }
+
+    public void setLastVehicle(Vehicle lastVehicle) {
+        this.lastVehicle = lastVehicle;
+    }
+
+    public List<Vehicle> getVehicleForSolution() {
+        return VehicleForSolution;
+    }
+
+    public void setVehicleForSolution(List<Vehicle> vehicleForSolution) {
+        VehicleForSolution = vehicleForSolution;
+    }
+}
