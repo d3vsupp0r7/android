1e81d1429ecd6fe033b3a23e149157d2cd80842a
d3vsupp0r7 <dev@dev.com>
2019-08-17 12:12:56 +0200

    Structure for investigation on Retrofit

---
+++ AndroidSimpleTrainer001/app/src/main/java/org/lba/android/simple/trainer/activity/retrofit/service/impl/RetrofitServiceImpl.java
@@ -0,0 +1,6 @@
+package org.lba.android.simple.trainer.activity.retrofit.service.impl;
+
+import org.lba.android.simple.trainer.activity.retrofit.service.RetrofitService;
+
+public class RetrofitServiceImpl implements RetrofitService {
+}
