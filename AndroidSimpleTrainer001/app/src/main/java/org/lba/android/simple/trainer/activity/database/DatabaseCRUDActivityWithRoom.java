d8a472ade6cb86ad8f30b9e7154974bd6b10b518
d3vsupp0r7 <d3vsupp0r7@example.com>
2019-08-30 04:22:45 +0200

    ROOM - Added example activity structure

---
+++ AndroidSimpleTrainer001/app/src/main/java/org/lba/android/simple/trainer/activity/database/DatabaseCRUDActivityWithRoom.java
@@ -0,0 +1,15 @@
+package org.lba.android.simple.trainer.activity.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+import org.lba.android.simple.trainer.R;
+
+import android.os.Bundle;
+
+public class DatabaseCRUDActivityWithRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_crud_with_room);
+    }
+}
